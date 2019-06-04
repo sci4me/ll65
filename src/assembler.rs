fe20c5a2381da2f35fc76d729b9ec8198478198f
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-06-04 12:09:34 -0400

    Work on Assembler

--- src/assembler.rs
+++ src/assembler.rs
@@ -60,6 +60,51 @@ impl Assembler {
         self.writer.put_u8(OpCode::ADC_INZP as u8);
         self.writer.put_u8(address);
     }
+    
+    pub fn and_absolute(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::AND_AB as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn and_absolute_x(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::AND_ABX as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn and_absolute_y(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::AND_ABY as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn and_immediate(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::AND_IMM as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn and_indirect_x(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::AND_INX as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn and_indirect_y(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::AND_INY as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn and_zp(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::AND_ZP as u8);
+        self.writer.put_u8(address);
+    }
+
+    pub fn and_zpx(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::AND_ZPX as u8);
+        self.writer.put_u8(address);
+    }
+
+    pub fn and_indirect_zp(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::AND_INZP as u8);
+        self.writer.put_u8(address);
+    }
 }
 
 #[cfg(test)]
@@ -145,5 +190,86 @@ mod tests {
         subject.adc_indirect_zp(42);
 
         assert_eq!(subject.assemble(), &[OpCode::ADC_INZP as u8, 42]);
+    }
+
+    #[test]
+    fn and_absolute_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_absolute(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_AB as u8, 0, 1]);
+    }
+
+    #[test]
+    fn and_absolute_x_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_absolute_x(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_ABX as u8, 0, 1]);
+    }
+
+    #[test]
+    fn and_absolute_y_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_absolute_y(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_ABY as u8, 0, 1]);
+    }
+
+    #[test]
+    fn and_immediate_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_immediate(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_IMM as u8, 42]);
+    }
+
+    #[test]
+    fn and_indirect_x_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_indirect_x(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_INX as u8, 42]);
+    }
+
+    #[test]
+    fn and_indirect_y_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_absolute_y(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_ABY as u8, 0, 1]);
+    }
+
+    #[test]
+    fn and_zp_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_zp(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_ZP as u8, 42]);
+    }
+
+    #[test]
+    fn and_zpx_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_zpx(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_ZPX as u8, 42]);
+    }
+
+    #[test]
+    fn and_indirect_zp_works() {
+        let mut subject = Assembler::new();
+
+        subject.and_indirect_zp(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::AND_INZP as u8, 42]);
     }
 }
