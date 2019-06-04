e1459f5700fc24e33a5d6acb55ff43c016db1822
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-06-04 11:13:38 -0400

    Work on Assembler

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,4 +1,5 @@
 use crate::binary_writer::BinaryWriter;
+use crate::opcodes::OpCode;
 
 pub struct Assembler {
     writer: BinaryWriter
@@ -13,5 +14,136 @@ impl Assembler {
 
     pub fn assemble(&self) -> &[u8] {
         self.writer.as_bytes()
+    }
+
+    pub fn adc_absolute(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::ADC_AB as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn adc_absolute_x(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::ADC_ABX as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn adc_absolute_y(&mut self, address: u16) {
+        self.writer.put_u8(OpCode::ADC_ABY as u8);
+        self.writer.put_u16(address);
+    }
+
+    pub fn adc_immediate(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::ADC_IMM as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn adc_indirect_x(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::ADC_INX as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn adc_indirect_y(&mut self, value: u8) {
+        self.writer.put_u8(OpCode::ADC_INY as u8);
+        self.writer.put_u8(value);
+    }
+
+    pub fn adc_zp(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::ADC_ZP as u8);
+        self.writer.put_u8(address);
+    }
+
+    pub fn adc_zpx(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::ADC_ZPX as u8);
+        self.writer.put_u8(address);
+    }
+
+    pub fn adc_indirect_zp(&mut self, address: u8) {
+        self.writer.put_u8(OpCode::ADC_INZP as u8);
+        self.writer.put_u8(address);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adc_absolute_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_absolute(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_AB as u8, 0, 1]);
+    }
+
+    #[test]
+    fn adc_absolute_x_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_absolute_x(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_ABX as u8, 0, 1]);
+    }
+
+    #[test]
+    fn adc_absolute_y_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_absolute_y(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_ABY as u8, 0, 1]);
+    }
+
+    #[test]
+    fn adc_immediate_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_immediate(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_IMM as u8, 42]);
+    }
+
+    #[test]
+    fn adc_indirect_x_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_indirect_x(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_INX as u8, 42]);
+    }
+
+    #[test]
+    fn adc_indirect_y_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_absolute_y(256);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_ABY as u8, 0, 1]);
+    }
+
+    #[test]
+    fn adc_zp_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_zp(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_ZP as u8, 42]);
+    }
+
+    #[test]
+    fn adc_zpx_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_zpx(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_ZPX as u8, 42]);
+    }
+
+    #[test]
+    fn adc_indirect_zp_works() {
+        let mut subject = Assembler::new();
+
+        subject.adc_indirect_zp(42);
+
+        assert_eq!(subject.assemble(), &[OpCode::ADC_INZP as u8, 42]);
     }
 }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,3 +1,31 @@
+// Opcode constants that have multiple addressing modes are named in the form
+// X_Y, where X is the instruction and Y is the addressing mode. The possible
+// addressing modes are:
+//       AB:  absolute mode, next two bytes are the low/high byte of an absolute
+//            memory address
+//      ABX:  absolute,X, next two bytes are added to the value in register X to
+//            get the memory address
+//      ABY:  same as ABX, except the value of register Y is used instead of X
+//      ACC:  accumulator, act on the value in the accumulator
+//      IMM:  immediate, next byte is a constant to be used instead of a lookup
+//       IN:  indirect, next two bytes are an absolute memory address of the
+//            lower nibble of a memory address. That byte and the byte after will
+//            be loaded and the address made of those two bytes will be used
+//      INX:  (indirect,X) mode, add X to the following byte, modulo 0xFF, and
+//            lookup two bytes starting at that location. Those two bytes form
+//            the memory address that will be used
+//      INY:  (indirect),Y mode, look up two bytes starting at address in the
+//            following byte, add Y modulo 0xFFFF, and use the result as an
+//            address
+//	   INZP:  indirect zero-page, next byte is a memory address within the zero-page
+//			  the byte at that address and the byte after it form a memory address
+//			  that address is where the actual value is loaded from
+//      REL:  relative, next byte contains a signed offset from the current PC.
+//       ZP:  zero-page, next byte is the low bits of the memory address (can
+//            only address first 256 bytes of memory using ZP)
+//      ZPX:  zero-page,X, add next byte to X modulo 0xFF and use that as a
+//            memory address
+
 #[allow(non_camel_case_types)]
 pub enum OpCode {
     ADC_AB = 0x6D,
@@ -9,6 +37,7 @@ pub enum OpCode {
     ADC_ZP = 0x65,
     ADC_ZPX = 0x75,
     ADC_INZP = 0x72,
+
     AND_AB = 0x2D,
     AND_ABX = 0x3D,
     AND_ABY = 0x39,
@@ -18,26 +47,34 @@ pub enum OpCode {
     AND_ZP = 0x25,
     AND_ZPX = 0x35,
     AND_INZP = 0x32,
+
     ASL_AB = 0x0E,
     ASL_ABX = 0x1E,
     ASL_ACC = 0x0A,
     ASL_ZP = 0x06,
     ASL_ZPX = 0x16,
+
     BCC_REL = 0x90,
     BCS_REL = 0xB0,
     BEQ_REL = 0xF0,
+
     BIT_AB = 0x2C,
     BIT_ZP = 0x24,
+
     BMI_REL = 0x30,
     BNE_REL = 0xD0,
     BPL_REL = 0x10,
+
     BRK = 0x00,
+
     BVC_REL = 0x50,
     BVS_REL = 0x70,
+
     CLC = 0x18,
     CLD = 0xD8,
     CLI = 0x58,
     CLV = 0xB8,
+
     CMP_AB = 0xCD,
     CMP_ABX = 0xDD,
     CMP_ABY = 0xD9,
@@ -47,19 +84,24 @@ pub enum OpCode {
     CMP_ZP = 0xC5,
     CMP_ZPX = 0xD5,
     CMP_INZP = 0xD2,
+
     CPX_AB = 0xEC,
     CPX_IMM = 0xE0,
     CPX_ZP = 0xE4,
+
     CPY_AB = 0xCC,
     CPY_IMM = 0xC0,
     CPY_ZP = 0xC4,
+    
     DEC_ACC = 0x3A,
     DEC_AB = 0xCE,
     DEC_ABX = 0xDE,
     DEC_ZP = 0xC6,
     DEC_ZPX = 0xD6,
+
     DEX = 0xCA,
     DEY = 0x88,
+
     EOR_AB = 0x4D,
     EOR_ABX = 0x5D,
     EOR_ABY = 0x59,
@@ -69,17 +111,22 @@ pub enum OpCode {
     EOR_ZP = 0x45,
     EOR_ZPX = 0x55,
     EOR_INZP = 0x52,
+
     INC_ACC = 0x1A,
     INC_AB = 0xEE,
     INC_ABX = 0xFE,
     INC_ZP = 0xE6,
     INC_ZPX = 0xF6,
+
     INX = 0xE8,
     INY = 0xC8,
+
     JMP_AB = 0x4C,
     JMP_ABX = 0x7C,
     JMP_IN = 0x6C,
+
     JSR_AB = 0x20,
+
     LDA_AB = 0xAD,
     LDA_ABX = 0xBD,
     LDA_ABY = 0xB9,
@@ -89,21 +136,25 @@ pub enum OpCode {
     LDA_ZP = 0xA5,
     LDA_ZPX = 0xB5,
     LDA_INZP = 0xB2,
+
     LDX_AB = 0xAE,
     LDX_ABY = 0xBE,
     LDX_IMM = 0xA2,
     LDX_ZP = 0xA6,
     LDX_ZPY = 0xB6,
+
     LDY_AB = 0xAC,
     LDY_ABX = 0xBC,
     LDY_IMM = 0xA0,
     LDY_ZP = 0xA4,
     LDY_ZPX = 0xB4,
+
     LSR_AB = 0x4E,
     LSR_ABX = 0x5E,
     LSR_ACC = 0x4A,
     LSR_ZP = 0x46,
     LSR_ZPX = 0x56,
+
     ORA_IMM = 0x09,
     ORA_ZP = 0x05,
     ORA_ZPX = 0x15,
@@ -113,23 +164,29 @@ pub enum OpCode {
     ORA_INX = 0x01,
     ORA_INY = 0x11,
     ORA_INZP = 0x12,
+
     NOP = 0xEA,
+
     PHA = 0x48,
     PHP = 0x08,
     PLA = 0x68,
     PLP = 0x28,
+
     ROL_AB = 0x2E,
     ROL_ABX = 0x3E,
     ROL_ACC = 0x2A,
     ROL_ZP = 0x26,
     ROL_ZPX = 0x36,
+
     ROR_AB = 0x6E,
     ROR_ABX = 0x7E,
     ROR_ACC = 0x6A,
     ROR_ZP = 0x66,
     ROR_ZPX = 0x76,
+
     RTI = 0x40,
     RTS = 0x60,
+
     SBC_IMM = 0xE9,
     SBC_ZP = 0xE5,
     SBC_ZPX = 0xF5,
@@ -139,9 +196,11 @@ pub enum OpCode {
     SBC_INX = 0xE1,
     SBC_INY = 0xF1,
     SBC_INZP = 0xF2,
+
     SEC = 0x38,
     SED = 0xF8,
     SEI = 0x78,
+
     STA_AB = 0x8D,
     STA_ABX = 0x9D,
     STA_ABY = 0x99,
@@ -150,19 +209,24 @@ pub enum OpCode {
     STA_ZP = 0x85,
     STA_ZPX = 0x95,
     STA_INZP = 0x92,
+
     STX_ZP = 0x86,
     STX_ZPY = 0x96,
     STX_AB = 0x8E,
+
     STY_ZP = 0x84,
     STY_ZPX = 0x94,
     STY_AB = 0x8C,
+
     TAX = 0xAA,
     TAY = 0xA8,
     TSX = 0xBA,
     TXA = 0x8A,
     TXS = 0x9A,
     TYA = 0x98,
+
     BRA = 0x80,
+
     BBR0 = 0x0F,
     BBR1 = 0x1F,
     BBR2 = 0x2F,
@@ -171,6 +235,7 @@ pub enum OpCode {
     BBR5 = 0x5F,
     BBR6 = 0x6F,
     BBR7 = 0x7F,
+
     BBS0 = 0x8F,
     BBS1 = 0x9F,
     BBS2 = 0xAF,
@@ -179,6 +244,7 @@ pub enum OpCode {
     BBS5 = 0xDF,
     BBS6 = 0xEF,
     BBS7 = 0xFF,
+
     RMB0 = 0x07,
     RMB1 = 0x17,
     RMB2 = 0x27,
@@ -187,6 +253,7 @@ pub enum OpCode {
     RMB5 = 0x57,
     RMB6 = 0x67,
     RMB7 = 0x77,
+
     SMB0 = 0x87,
     SMB1 = 0x97,
     SMB2 = 0xA7,
@@ -195,18 +262,23 @@ pub enum OpCode {
     SMB5 = 0xD7,
     SMB6 = 0xE7,
     SMB7 = 0xF7,
+
     TRB_AB = 0x1C,
     TRB_ZP = 0x14,
+
     TSB_AB = 0x0C,
     TSB_ZP = 0x04,
+
     STZ_AB = 0x9C,
     STZ_ABX = 0x9E,
     STZ_ZP = 0x64,
     STZ_ZPX = 0x74,
+
     PHX = 0xDA,
     PHY = 0x5A,
     PLX = 0xFA,
     PLY = 0x7A,
+
     WAI = 0xCB,
     STP = 0xDB
 }
